1b99697194045a6c3b0c7a350234c0abb65c9a03
Sai Deepak <68103212+Saideepak01@users.noreply.github.com>
2022-06-17 12:46:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001
+
+// API for get requests
+async function getreq({
+    try{
+        let data = await fetch("https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001")
+    }
+})
